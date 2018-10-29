591bbf1035f31c07a9ce2790bf49a8aa310cf576
yuz1988 <yuz1988@iastate.edu>
2018-10-29 15:01:51 -0500

    Merge read-link and read-group-membership python files together, now each user id is converted to dense representation from 0 to n-1 (n users). Observation: using matlab to generate sparse matrix in .mat file is smaller than using scipy. Though this script can process the youtube and flickr datasets to .mat file, the cleaned processed files have already been ready for downloading here: http://leitang.net/social_dimension.html

--- src/convert-sparse-mat.py
+++ src/convert-sparse-mat.py
@@ -4,44 +4,38 @@ from scipy import sparse
 
 NUM_USER = 1138499
 GROUPS = 47
-network_file_name = "../output/cleaned-youtube-links.txt"
+link_file_name = "../output/cleaned-youtube-links.txt"
 group_file_name = "../output/grouping.txt"
 
-# # create three lists: row, column, value
-# user_list, group_list, value_list = [], [], []
-# with open(group_file_name, 'r') as f:
-#     lines = f.readlines()
-#     for line in lines:
-#         vertices = re.split('\s+', line)
-#         user_list.append(int(vertices[0])-1)   # user starts from 0
-#         group_list.append(int(vertices[1])-1)  # group number starts from 0
-#         value_list.append(1)
-#
-# # for i in range(len(user_list)):
-# #     print(str(user_list[i]) + " " + str(group_list[i]) + " " + str(value_list[i]))
-#
-# # convert to sparse matrix
-# sparse_matrix = sparse.csr_matrix((value_list, (user_list, group_list)),
-#                                   shape=(NUM_USER, GROUPS))
-# scipy.io.savemat('youtube.mat', mdict={'group': sparse_matrix})
-
-
-
-############################################
-from_list, to_list, value_list = [], [], []
-with open(network_file_name, 'r') as f:
+# Convert group membership to .mat file.
+# Create three lists: row, column, value
+user_list, group_list, value_list = [], [], []
+with open(group_file_name, 'r') as f:
     lines = f.readlines()
     for line in lines:
         vertices = re.split('\s+', line)
-        from_list.append(int(vertices[0]) - 1)  # user starts from 0
-        to_list.append(int(vertices[1]) - 1)  # group number starts from 0
+        user_list.append(int(vertices[0]))
+        group_list.append(int(vertices[1]))
+        value_list.append(1)
+
+# convert to sparse matrix
+group_sparse_matrix = sparse.csr_matrix((value_list, (user_list, group_list)),
+                                        shape=(NUM_USER, GROUPS))
+
+# Convert links to .mat file.
+from_list, to_list, value_list = [], [], []
+with open(link_file_name, 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        vertices = re.split('\s+', line)
+        from_list.append(int(vertices[0]))  # user starts from 0
+        to_list.append(int(vertices[1]))  # group number starts from 0
         value_list.append(1)
         # symmetry
-        from_list.append(int(vertices[1]) - 1)  # user starts from 0
-        to_list.append(int(vertices[0]) - 1)  # group number starts from 0
+        from_list.append(int(vertices[1]))  # user starts from 0
+        to_list.append(int(vertices[0]))  # group number starts from 0
         value_list.append(1)
 
 # convert to sparse matrix
 network_sparse_matrix = sparse.csr_matrix((value_list, (from_list, to_list)), shape=(NUM_USER, NUM_USER))
-scipy.io.savemat('network.mat', mdict={'network': network_sparse_matrix})
-
+scipy.io.savemat('youtube.mat', mdict={'group': group_sparse_matrix, 'network': network_sparse_matrix})
--- src/read-link.py
+++ src/read-link.py
@@ -2,13 +2,17 @@ import re
 
 # input file name
 in_file_name = '../data/release-youtube-links.txt'
+in_group_file_name = '../data/release-youtube-groupmemberships.txt'
 
 # output file name
 out_link_file_name = '../output/cleaned-youtube-links.txt'
 out_user_mapping_file_name = '../output/user-sparse-to-dense-mapping.txt'
+out_group_file_name = "../output/grouping.txt"
+
+THRESHOLD = 500
 
 user_set = set()
-adj_map = {}   # adjacency list
+adj_map = {}  # adjacency map
 
 with open(in_file_name) as f:
     lines = f.readlines()
@@ -28,7 +32,7 @@ with open(in_file_name) as f:
 
 print("Number of distinct users: {}\n".format(len(user_set)))
 
-# create sparse to dense (consecutive from 0) user mapping.
+# Create sparse to dense (consecutive from 0) user mapping.
 user_list = list(user_set)
 user_list.sort()
 sparse_to_dense_user_mapping = {}
@@ -40,8 +44,7 @@ with open(out_user_mapping_file_name, 'w') as out:
         out.write("{} {}\n".format(user_list[i], i))
 print("User sparse to dense mapping file has been written.\n")
 
-
-# generate compressed adjacency list.
+# Generate compressed adjacency list.
 compressed_adj_list = []
 for i in range(len(user_list)):
     neighbor_list = adj_map[dense_to_sparse_user_mapping[i]]
@@ -51,9 +54,53 @@ for i in range(len(user_list)):
     compressed_user_list.sort()
     compressed_adj_list.append(compressed_user_list)
 
-# write compressed links to file, keep links: from_v < to_v.
+# Write compressed links to file, keep links: from_v < to_v.
 with open(out_link_file_name, 'w') as output_file:
     for i in range(len(compressed_adj_list)):
         for j in compressed_adj_list[i]:
             if i < j:
                 output_file.write("{} {}\n".format(i, j))
+
+# Process group membership.
+group_map = {}
+with open(in_group_file_name) as f:
+    lines = f.readlines()
+    for line in lines:
+        vertices = re.split('\s+', line)
+        user = int(vertices[0])
+        group = int(vertices[1])
+        if user in user_set:
+            if group not in group_map:
+                group_map[group] = []
+            group_map[group].append(sparse_to_dense_user_mapping[user])
+
+
+print("Total groups: {}\n".format(len(group_map)))
+
+top_groups_list = []
+top_group_users = set()
+for k, v in group_map.items():
+    if len(v) >= THRESHOLD:
+        top_groups_list.append(k)
+        for user in v:
+            top_group_users.add(user)
+
+print("Groups with more than {} users: {}\n".format(THRESHOLD, len(top_groups_list)))
+print("Number of users in top groups: {}\n".format(len(top_group_users)))
+
+# Convert top group numbers to from 1 to n (n = len(top_groups))
+top_N = len(top_groups_list)
+top_groups_list.sort()
+group_mapping_dict = {}
+for i in range(top_N):
+    group_mapping_dict[top_groups_list[i]] = i
+
+with open(in_group_file_name) as f, open(out_group_file_name, 'w') as output_file:
+    lines = f.readlines()
+    for line in lines:
+        vertices = re.split('\s+', line)
+        user = int(vertices[0])
+        group = int(vertices[1])
+        if (group in group_mapping_dict) and (user in user_set):
+            output_file.write("{} {}\n".format(sparse_to_dense_user_mapping[user],
+                                               group_mapping_dict[group]))
